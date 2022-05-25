35547777270f8d85fb6e1aaf2f55ba7e002d5b3a
vladkotulla <71208449+vladkotulla@users.noreply.github.com>
2022-05-25 09:16:07 +0300

    Добавьте файлы проекта.

---
+++ Bookstore.Core/DbClient.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Bookstore.Core
+{
+    public class DbClient : IDbClient
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public DbClient(IOptions<BookstoreDbConfig> bookstoreDbConfig)
+        {
+            var client = new MongoClient(bookstoreDbConfig.Value.Connection_String);
+            var database = client.GetDatabase(bookstoreDbConfig.Value.Database_Name);
+            _books = database.GetCollection<Book>(bookstoreDbConfig.Value.Books_Collection_Name);
+        }
+
+        public IMongoCollection<Book> GetBooksCollection() => _books;
+    }
+}
